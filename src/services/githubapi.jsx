462309e5eb1253b5b2e8faa1774d3ebf39133e33
micherenio <micherenio.nascimento@gmail.com>
2024-09-06 17:33:47 -0300

    Atribuindo ao componente githubapi apenas a função de buscar

--- src/services/githubapi.jsx
+++ src/services/githubapi.jsx
@@ -1,35 +1,7 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-
-const Search = () => {
-
-    const [repos, setRepos] = useState([])
-
-    useEffect(() => {
-  
-      fetch('https://api.github.com/users/micherenio-nascimento/repos')
-      .then(res => res.json())
-      // .then(data => console.log(data))
-      .then(data => setRepos(data))
-  
-    },[])
-  
-    return (
-      <>
-        <div>
-          <ul>
-            { repos.map(repo => {
-              return (
-                <li>
-                  <h3>{repo.name}</h3>
-                  <p>{repo.language}</p>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      </>
-    )
-}
-
-export default Search
+export const fetchRepos = async (username) => {
+  const response = await fetch(`https://api.github.com/users/${username}/repos`);
+  if (!response.ok) {
+    throw new Error('Erro ao buscar repositórios');
+  }
+  return response.json();
+};
