5b87314ad2f8587970175edc5131052783c3bbaa
micherenio <micherenio.nascimento@gmail.com>
2024-09-06 17:43:35 -0300

    O componente App para centralizar os outros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import { useEffect, useState } from "react"
+import Home from "./pages/Home"
 
 export default function App() {
 
-  const [repos, setRepos] = useState([])
-
-  useEffect(() => {
-
-    fetch('https://api.github.com/users/micherenio-nascimento/repos')
-    .then(res => res.json())
-    // .then(data => console.log(data))
-    .then(data => setRepos(data))
-
-  },[])
-
   return (
     <>
-      <div>
-        <ul>
-          { repos.map(repo => {
-            return (
-              <li>
-                <h3>{repo.name}</h3>
-                <p>{repo.language}</p>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <Home/>
     </>
   )
 }
